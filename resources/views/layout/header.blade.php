2faac3c8f72616c128fadd7799b4822b839ba7d9
chidokato <tuan.pn92@gmail.com>
2025-12-16 11:01:02 +0700

    sdf

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -49,12 +49,15 @@
                     <div class="icon-header">
                         <a target="_blank" href="{{$setting->facebook}}"><img src="assets/images/facebook-logo-icon.png"></a>
                         <a target="_blank" href="{{$setting->youtube}}"><img src="assets/images/youtube-logo-icon.png"></a>
-                        <div class="dropdown-menu">
-                                <div class="mb-2"><a href="https://cn.vietnampropertymarket.com/"><img src="assets/images/lang/cn.avif"> 中文</a></div>
-                                <div class="mb-2"><a href="https://vietnampropertymarket.com/"><img src="assets/images/lang/en.avif"> English</a></div>
+                        <div class="dropdown language">
+                            <a title="" target="{{ asset('') }}" href=""><img src="assets/images/lang/kr.avif"></a>
+                            <a class="expand dropdown-toggle d-lg-none" href="#" data-bs-toggle="dropdown"></a>
+                            <div class="dropdown-menu">
                                 <div class="mb-2"><a href="https://kr.vietnampropertymarket.com/"><img src="assets/images/lang/kr.avif"> 한국어</a></div>
-                                
+                                <div class="mb-2"><a href="https://vietnampropertymarket.com/"><img src="assets/images/lang/en.avif"> English</a></div>
+                                <div><a href="https://cn.vietnampropertymarket.com/"><img src="assets/images/lang/cn.avif"> 中文</a></div>
                             </div>
+                        </div>
                     </div>
                 </li>
                 <!-- <li class="nav-item dropdown">
